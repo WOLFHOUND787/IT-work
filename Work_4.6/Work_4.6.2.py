d9434eeba087d861295ea0cb0859729237b3296e
Ярослав Осипов <Oyaroslav2001@gmail.com>
2022-11-10 17:51:57 +0700

    Update Work_4.6.2.py

---
+++ Work_4.6/Work_4.6.2.py
@@ -0,0 +1,9 @@
+from random import randint
+
+number1 = float(randint(0, 5))
+number2 = float(randint(1, 5)) #Деление на 0
+
+result = [number1 // number2, number1 % number2]
+print(result)
+
+#Ещё не готова
