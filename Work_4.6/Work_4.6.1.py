e79af7fcc290eabec3b347c983f38e38f5dce606
Ярослав Осипов <Oyaroslav2001@gmail.com>
2022-11-10 17:24:08 +0700

    4.6

---
+++ Work_4.6/Work_4.6.1.py
@@ -0,0 +1,5 @@
+number1 = float(input("Введите первое число:\n")) #Задание переменных в формате float
+number2 = float(input("Введите второе число:\n"))
+number3 = float(input("Введите третее число:\n"))
+average = (number1+number2+number3)/3   #Формула среднего значения
+print("Среднее значение: {0:.2f}".format(average))  #Вывод среднего значения с округлением до сотых
